ebbcdc62f0ed107b1ca9b9ccc8b6419993e0723c
Thiago Moreira <thiago.moreira@askblue.pt>
2024-04-04 14:30:17 +0100

    [TM] - Update

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,12 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Localization;
+using System.Resources;
 
 namespace WebApp.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
+        public string? Teste { get; set; }
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -14,7 +15,8 @@ namespace WebApp.Pages
 
         public void OnGet()
         {
-
+            var rm = new ResourceManager(typeof(Resource));
+            Teste = rm.GetString("teste");
         }
     }
 }
